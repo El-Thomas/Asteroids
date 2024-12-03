47fefd673018783e8c16ffa2276efffaf04860ab
ThomasSpoelstra <Thomas_spoel@hotmail.com>
2024-12-03 16:51:44 +0100

    added game ticks for cpu optimalisation

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ from constants import *
 
 def main(): 
     pygame.init()
+
     pygame.display.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
     
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -20,7 +24,10 @@ def main():
                 return
             
         screen.fill("black")
-        pygame.display.flip
+        pygame.display.flip()
+        
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
