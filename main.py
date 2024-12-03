8aeab5e0225168befab82e51f0ece00cb200dc70
ThomasSpoelstra <Thomas_spoel@hotmail.com>
2024-12-03 20:31:52 +0100

    rotation of the player by keys

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def main():
 
     pygame.display.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
     dt = 0
 
@@ -22,12 +21,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        player.update(dt)    
+        
         screen.fill("black")
+
         player.draw(screen)
         pygame.display.flip()
         
-        clock.tick(60)
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
