00b030f47608aeb221b989173880c454e04706d4
ThomasSpoelstra <Thomas_spoel@hotmail.com>
2024-12-03 19:57:42 +0100

    player class init and logics

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main(): 
     pygame.init()
@@ -9,6 +10,8 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -21,11 +24,11 @@ def main():
                 return
             
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
         clock.tick(60)
         dt = clock.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
